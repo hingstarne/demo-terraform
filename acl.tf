fa134040558491a707e3de7e542c93578f8a6dc9
HIngst, Arne-Kristian <arne@hingst.it>
2016-02-15 00:37:18 +0100

    Changed to ingress and egress subrules

--- acl.tf
+++ acl.tf
@@ -1,20 +1,22 @@
 resource "aws_network_acl" "main" {
     vpc_id = "${aws_vpc.main.id}"
-    egress {
-        protocol = "-1"
-        rule_no = 100
-        action = "allow"
-        cidr_block =  "0.0.0.0/0"
-    }
-
-    ingress {
-        protocol = "-1"
-        rule_no = 100
-        action = "allow"
-        cidr_block =  "0.0.0.0/0"
-    }
-
     tags {
         Name = "${var.vpc_name}-acl"
     }
+}
+resource "aws_network_acl_rule" "egress" {
+    network_acl_id = "${aws_network_acl.main.id}"
+    rule_number = 100
+    egress = true
+    protocol = "-1"
+    rule_action = "allow"
+    cidr_block = "0.0.0.0/0"
+}
+resource "aws_network_acl_rule" "ingress" {
+    network_acl_id = "${aws_network_acl.main.id}"
+    rule_number = 100
+    egress = false
+    protocol = "-1"
+    rule_action = "allow"
+    cidr_block = "0.0.0.0/0"
 }
