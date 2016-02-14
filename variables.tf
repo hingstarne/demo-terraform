c134a263ea0dcb09646e3ba447fa0bc70769d37b
HIngst, Arne-Kristian <arne@hingst.it>
2016-02-14 23:56:28 +0100

    Added freeletics subnets and namings

--- variables.tf
+++ variables.tf
@@ -9,6 +9,11 @@ variable "secret_key" {
     description = "Please enter your AWS secret key"
 }
 
+variable "vpc_name" {
+    description = "Please enter your VPC name"
+    default = "demo"
+}
+
 variable "vpc_region" {
     description = "Please enter your AWS region"
     default = "eu-west-1"
--- vpc.tf
+++ vpc.tf
@@ -4,16 +4,43 @@
 resource "aws_vpc" "main" {
     cidr_block = "10.100.0.0/16"
     enable_dns_hostnames = true
+    tags {
+        Name = "${var.vpc_name}"
+    }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "Subnet1a" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.100.0.0/24"
-    map_public_ip_on_launch = true
+    availability_zone = "eu-west-1a"
+    tags {
+        Name = "${var.vpc_name}-subnet-1a"
+    }
+}
+
+resource "aws_subnet" "Subnet1b" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.100.1.0/24"
+    availability_zone = "eu-west-1b"
+    tags {
+        Name = "${var.vpc_name}-subnet-1b"
+    }
+}
+
+resource "aws_subnet" "Subnet1c" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.100.2.0/24"
+    availability_zone = "eu-west-1c"
+    tags {
+        Name = "${var.vpc_name}-subnet-1c"
+    }
 }
 
 resource "aws_internet_gateway" "gw" {
     vpc_id = "${aws_vpc.main.id}"
+    tags {
+        Name = "${var.vpc_name}-igw"
+    }
 }
 
 resource "aws_route_table" "r" {
@@ -21,6 +48,9 @@ resource "aws_route_table" "r" {
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+    tags {
+        Name = "${var.vpc_name}-rt"
     }
 }
 
