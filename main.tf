45e8bc81dac59fca98ea135eab7d05f835c909fa
HIngst, Arne-Kristian <arne@hingst.it>
2016-02-15 13:04:22 +0100

    Fixed wrong name in vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "github.com/terraform-community-modules/tf_aws_vpc"
 
-  name = "${var.secret_key}"
+  name = "${var.vpc_name}"
   cidr = "10.100.0.0/16"
   private_subnets = "10.100.1.0/24,10.100.2.0/24,10.100.3.0/24"
   public_subnets  = "10.100.101.0/24,10.100.102.0/24,10.100.103.0/24"
