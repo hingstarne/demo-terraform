d1c3cc369b74b20d97cb214cad5e795af9dbdee3
HIngst, Arne-Kristian <arne@hingst.it>
2016-02-15 13:22:14 +0100

    Added acls back in vpc

--- main.tf
+++ main.tf
@@ -9,3 +9,27 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support = true
 }
+
+resource "aws_network_acl" "main" {
+    vpc_id = "${module.vpc.vpc_id}"
+    tags {
+        Name = "${var.vpc_name}-acl"
+    }
+}
+
+resource "aws_network_acl_rule" "egress" {
+    network_acl_id = "${aws_network_acl.main.id}"
+    rule_number = 100
+    egress = true
+    protocol = "-1"
+    rule_action = "allow"
+    cidr_block = "0.0.0.0/0"
+}
+resource "aws_network_acl_rule" "ingress" {
+    network_acl_id = "${aws_network_acl.main.id}"
+    rule_number = 100
+    egress = false
+    protocol = "-1"
+    rule_action = "allow"
+    cidr_block = "0.0.0.0/0"
+}
