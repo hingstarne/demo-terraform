97c90d9f39129949863e3b827c60079e1db07fc4
HIngst, Arne-Kristian <arne@hingst.it>
2016-02-15 12:48:19 +0100

    Minimized Outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
 output "vpc_id" {
-    value = "${aws_vpc.main.id}"
-}
-
-output "vpc_subnet1a" {
-    value = "${aws_subnet.Subnet1a.id}"
-}
-
-output "vpc_subnet1b" {
-    value = "${aws_subnet.Subnet1b.id}"
-}
-
-output "vpc_subnet1c" {
-    value = "${aws_subnet.Subnet1c.id}"
+    value = "${module.vpc.vpc_id}"
 }
